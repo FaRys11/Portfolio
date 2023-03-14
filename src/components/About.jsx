07654689623e6370d3e8091d3622cc85c56c2ce1
Martin Gottwald <gott45@seznam.cz>
2023-03-14 19:47:33 +0100

    prace na about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Úvod</p>
+        <h2 className={styles.sectionHeadText}>Přehled</h2>
+      </motion.div>
+
+      <motion
+        varriants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Jsem začínající programator, který se převážně věnuje 3D, 2D návrhům webových aplikací a webových stránek. Využívám programovací jazyk JavaScript. Mám zkušenosti s frameworky, jako jsou React, Node.js a Three.js. Učím se rychle a úzce spolupracuji s klientem.
+      </motion>
+    </>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,12 +26,12 @@ const Hero = () => {
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2'>
             <motion.dev 
               animate={{
-                y:[0, 24, 0]
+                y:[0, 24, 0],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: 'loop'
+                repeatType: 'loop',
               }}
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
