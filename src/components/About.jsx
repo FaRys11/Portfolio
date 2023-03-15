709397bb12048a6b28c49ccf12e75a100378698c
Martin Gottwald <gott45@seznam.cz>
2023-03-15 20:23:25 +0100

    pokračování na about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,31 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"      
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className='text-white text-[20px] font-bold text-center'>
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const About = () => {
   return (
     <>
@@ -14,12 +39,18 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Přehled</h2>
       </motion.div>
 
-      <motion
+      <motion.p
         varriants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         Jsem začínající programator, který se převážně věnuje 3D, 2D návrhům webových aplikací a webových stránek. Využívám programovací jazyk JavaScript. Mám zkušenosti s frameworky, jako jsou React, Node.js a Three.js. Učím se rychle a úzce spolupracuji s klientem.
-      </motion>
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
