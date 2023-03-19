37d84fafb86dbef40fda2c4d28d69fd342b299a1
Martin Gottwald <gott45@seznam.cz>
2023-03-19 17:30:13 +0100

    experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,27 +8,31 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
-const ExperienceCard = ({ experience }) => {
+const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
-    contentStyle={{background: '#1d1836', color: '#fff'}}
-    contentArrowStyle={{ borderRight: '7px solid  #232631' }}
-    date={experience.date}
-    iconStyle={{ background: 'exprience.iconBg' }}
-    icon={
-      <div>
-        <img 
-          src={experience.icon}
-          alt={experience.company_name}
-          className="w-[60%] h-[60%] object-contain"
-        />  
-      </div>
-    }
-  >
+  contentStyle={{
+    background: "#1d1836",
+    color: "#fff",
+  }}
+  contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+  date={experience.date}
+  iconStyle={{ background: experience.iconBg }}
+  icon={
+    <div className='flex justify-center items-center w-full h-full'>
+      <img
+        src={experience.icon}
+        alt={experience.company_name}
+        className='w-[60%] h-[60%] object-contain'
+      />
+    </div>
+  }
+>
     <div>
-      <h3>{experience.title}</h3>
+      <h3 className='text-white text-[24px] text-bold'>{experience.title}</h3>
+      <p className='text-secondary text-[16px] font-semibold' style={{ margin: 0 }}>{experience.company_name}</p>
     </div>
   </VerticalTimelineElement>
-}
+)
 
 const Experience = () => {
   return (
